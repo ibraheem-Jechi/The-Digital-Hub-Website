455310a8a841fd1bff025a694cb7e4e5e985bdb6
Abdelhadi Shmas <abdshams78@gmail.com>
2025-09-30 14:24:04 +0300

    solve small problem

--- database/migrations/2025_09_25_080757_create_sponsorships_table.php
+++ database/migrations/2025_09_25_080757_create_sponsorships_table.php
@@ -4,21 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateSponsersTable extends Migration
 {
-  
+
   public function up(): void
-{
+  {
     Schema::create('sponsorships', function (Blueprint $table) {
-        $table->id();
-        $table->string('description');
-        $table->string('logo_url')->nullable();
-        $table->string('website_url');
-        $table->timestamps();
+      $table->id();
+      $table->string('description');
+      $table->string('logo_url')->nullable();
+      $table->string('website_url');
+      $table->timestamps();
     });
+  }
+
+
+
+  public function down(): void
+  {
+    Schema::dropIfExists('sponsorships');
+  }
 }
-
-
-   
-  public function down(): void{Schema::dropIfExists('sponsorships');}
-};
+;
