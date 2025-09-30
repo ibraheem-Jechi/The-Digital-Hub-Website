a7cdc11d9178f0ba09497c7d2945096dcfd94912
Abdelhadi Shmas <abdshams78@gmail.com>
2025-09-30 14:33:38 +0300

    fix some issues

--- database/migrations/2025_09_24_074256_create_workshops_table.php
+++ database/migrations/2025_09_24_074256_create_workshops_table.php
@@ -4,16 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+class CreateWorkshopsTable extends Migration
+{
     public function up(): void
     {
         Schema::create('workshops', function (Blueprint $table) {
-            $table->id('workshop_id'); // Primary Key
+            $table->bigIncrements('workshop_id');
             $table->string('title');
             $table->text('description')->nullable();
             $table->date('event_date');
-            $table->unsignedBigInteger('program_id')->nullable(); // FK later
-            $table->string('image')->nullable(); // store image path or filename
+            $table->unsignedBigInteger('program_id')->nullable();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
@@ -22,4 +23,4 @@ return new class extends Migration {
     {
         Schema::dropIfExists('workshops');
     }
-};
+}
